95e62449bfec064893b4a5111e7c1da8fd4b3c6b
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-03 17:41:07 -0300

    Improve DJ Loritta response

--- src/test/kotlin/responses/portuguese/DJLorittaResponseTest.kt
+++ src/test/kotlin/responses/portuguese/DJLorittaResponseTest.kt
@@ -3,6 +3,7 @@ package responses.portuguese
 class DJLorittaResponseTest : PortugueseResponseTestBase(
     listOf(
         "Como bota dj loritta?",
-        "Alguém pode me ajudar a configurar o DJ da Loritta?"
+        "Alguém pode me ajudar a configurar o DJ da Loritta?",
+        "Queria saber a função do Dj loritta, queria deixar ela em um canal de voz específico e deixar tocando uma única música em 24h por dia, é possível ?"
     )
 )
