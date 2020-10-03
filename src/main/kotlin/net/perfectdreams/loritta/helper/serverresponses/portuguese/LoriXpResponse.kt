52715e2dfde824a34a505f6b71fc2182fc7be424
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-03 15:49:32 -0300

    Improve XP message detection

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/LoriXpResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/LoriXpResponse.kt
@@ -7,7 +7,7 @@ import java.util.regex.Pattern
 
 class LoriXpResponse : RegExResponse() {
     init {
-        patterns.add("ganh(a|o)|sobe|subi|pega|pego|dá|vejo|ver|saber".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("ganh(a|o)|sobe|subi|pega|pego|dá|vejo|ver|saber|calcul".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("(experi(ê|e)ncia|xp|n(í|i)vel)".toPattern(Pattern.CASE_INSENSITIVE))
     }
 
--- src/test/kotlin/responses/portuguese/LoriXpResponseTest.kt
+++ src/test/kotlin/responses/portuguese/LoriXpResponseTest.kt
@@ -4,6 +4,7 @@ class LoriXpResponseTest : PortugueseResponseTestBase(
     listOf(
         "Alguém pode de informar como faço pra saber em que nível estou e como faço pra upar de nível por favor?",
         "Como sobe de nível?",
-        "Como ganha experiência na Loritta?"
+        "Como ganha experiência na Loritta?",
+        "Então alguém pode me dizer como calcular o XP da Lorrita?"
     )
 )
