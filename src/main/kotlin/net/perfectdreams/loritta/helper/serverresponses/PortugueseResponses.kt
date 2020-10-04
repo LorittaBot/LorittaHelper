da32976b47e9e5a83760085a9a15095a08262830
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-03 21:40:53 -0300

    Add "how to transfer garticos" response

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/PortugueseResponses.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/PortugueseResponses.kt
@@ -38,6 +38,7 @@ object PortugueseResponses {
         SlowModeResponse(),
         StarboardResponse(),
         SugestoesResponse(),
+        TransferGarticosResponse(),
         ValorShipResponse(),
         VotarResponse(),
         WhoIsVieirinhaResponse(),
---
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/TransferGarticosResponse.kt
@@ -0,0 +1,27 @@
+package net.perfectdreams.loritta.helper.serverresponses.portuguese
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.perfectdreams.loritta.api.messages.LorittaReply
+import net.perfectdreams.loritta.helper.serverresponses.RegExResponse
+import net.perfectdreams.loritta.helper.utils.Emotes
+import java.util.regex.Pattern
+
+class TransferGarticosResponse : RegExResponse() {
+    init {
+        patterns.add(WHERE_IT_IS.toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("pass|transf|troc|funciona".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("gartic".toPattern(Pattern.CASE_INSENSITIVE))
+    }
+
+    override fun getResponse(event: GuildMessageReceivedEvent, message: String) =
+        listOf(
+            LorittaReply(
+                "Você pode transferir garticos por sonhos utilizando `gb.garticos Sonhos Quantia` em qualquer servidor que possua o GarticBot!",
+                Emotes.WUMPUS_KEYBOARD
+            ),
+            LorittaReply(
+                "Para mais informações, entre no servidor do Gartic! Você pode encontrar o convite em <#761956906368892958>",
+                Emotes.LORI_OWO
+            )
+        )
+}
