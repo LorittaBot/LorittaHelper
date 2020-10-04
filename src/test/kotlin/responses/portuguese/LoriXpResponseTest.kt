8c6d80abdad295212b03643412f3c813e1ed343e
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-03 22:25:25 -0300

    Improve XP response check

--- src/test/kotlin/responses/portuguese/LoriXpResponseTest.kt
+++ src/test/kotlin/responses/portuguese/LoriXpResponseTest.kt
@@ -5,6 +5,7 @@ class LoriXpResponseTest : PortugueseResponseTestBase(
         "Alguém pode de informar como faço pra saber em que nível estou e como faço pra upar de nível por favor?",
         "Como sobe de nível?",
         "Como ganha experiência na Loritta?",
-        "Então alguém pode me dizer como calcular o XP da Lorrita?"
+        "Então alguém pode me dizer como calcular o XP da Lorrita?",
+        "como eu sei de quanto de xp tenho"
     )
 )
