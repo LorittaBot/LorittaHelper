638e0e2b54dbd2cf27c05ab33bf7545fa1dba261
xGabrielDEV <58668092+xGabrielDEV@users.noreply.github.com>
2020-10-04 12:08:27 -0300

    Add more response types

---
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/SparklyPowerInfoResponse.kt
@@ -0,0 +1,22 @@
+package net.perfectdreams.loritta.helper.serverresponses.portuguese
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.perfectdreams.loritta.api.messages.LorittaReply
+import net.perfectdreams.loritta.helper.serverresponses.RegExResponse
+import net.perfectdreams.loritta.helper.utils.Constants
+import net.perfectdreams.loritta.helper.utils.Emotes
+import java.util.regex.Pattern
+
+class SparklyPowerInfoResponse: RegExResponse() {
+    
+    init {
+        patterns.add("sparkly".toPattern(Pattern.CASE_INSENSITIVE))
+    }
+
+    override fun getResponse(event: GuildMessageReceivedEvent, message: String): List<LorittaReply> = listOf(
+        LorittaReply(
+            message = "Psiu! Se você precisa de ajuda com coisas relatadas ao **SparklyPower:tm:**, recomendo que procure por ajuda no servidor direcionado a ele: ${Constants.SPARKLY_POWER_INVITE_CODE}!",
+            prefix = Emotes.WUMPUS_KEYBOARD
+        )
+    )
+}
---
+++ src/test/kotlin/responses/portuguese/LoriNameResponseTest.kt
@@ -0,0 +1,9 @@
+package responses.portuguese
+
+class LoriNameResponseTest: PortugueseResponseTestBase(
+    listOf(
+        "Eu tento fazer um comando na Lorrita",
+        "Eu amo a Lorri, ela é minha amiga!",
+        "A lorita não responde meus comandos..."
+    )
+)
