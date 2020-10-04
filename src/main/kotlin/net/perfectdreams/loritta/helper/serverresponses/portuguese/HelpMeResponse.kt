966abb4c6b5887cb39d6e696b5e6cc71ebcd354a
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-10-04 10:59:33 -0300

    Fix "Help Me Response" role in message check

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/HelpMeResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/HelpMeResponse.kt
@@ -15,15 +15,15 @@ class HelpMeResponse : RegExResponse() {
         patterns.add("algu?(e|é)?m|como|ninguém".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("ajud|d(ú|u)vida|help|faç|fass|coloco".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("\\?".toPattern(Pattern.CASE_INSENSITIVE))
-        }
+    }
 
     override fun getResponse(event: GuildMessageReceivedEvent, message: String) =
-            if (!message.contains(Constants.ENGLISH_SUPPORT_CHANNEL_ID.toString())) {
+            if (!message.contains(Constants.PORTUGUESE_LORITTA_SUPPORT_ROLE_ID.toString())) {
                 listOf(
-                    LorittaReply(
-                        "Psiu! Se você está com uma dúvida, escreva a sua dúvida no chat e marque o cargo do <@&${Constants.PORTUGUESE_LORITTA_SUPPORT_ROLE_ID}>!",
-                        Emotes.LORI_PAT
-                    )
+                        LorittaReply(
+                                "Psiu! Se você está com uma dúvida, escreva a sua dúvida no chat e marque o cargo do <@&${Constants.PORTUGUESE_LORITTA_SUPPORT_ROLE_ID}>!",
+                                Emotes.LORI_PAT
+                        )
                 )
             } else listOf()
 }
