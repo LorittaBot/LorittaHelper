17eb04299a5329e46b94ec2cbeaea5d5dfa2ad80
joaoesteves10 <joaoesteves10A4@sapo.pt>
2020-10-06 13:49:50 +0100

    Update src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/english/WhoIsVieirinhaResponse.kt
    Co-authored-by: Gabriel <58668092+xGabrielDEV@users.noreply.github.com>

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/english/WhoIsVieirinhaResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/english/WhoIsVieirinhaResponse.kt
@@ -18,7 +18,7 @@ class WhoIsVieirinhaResponse : RegExResponse() {
 
     override fun getResponse(event: GuildMessageReceivedEvent, message: String) = listOf(
         LorittaReply(
-            "A friend of mine who knows `@MrPowerGamerBR#4185` (my creator and dad :3) in real life, he's a good guy who likes to paly Minecraft and enjoy life :3",
+            "A friend of mine who knows `@MrPowerGamerBR#4185` (my creator and dad :3) in real life, he's a good guy who likes to play Minecraft and enjoy life :3",
             Emotes.SUPER_VIEIRINHA
         )
     )
