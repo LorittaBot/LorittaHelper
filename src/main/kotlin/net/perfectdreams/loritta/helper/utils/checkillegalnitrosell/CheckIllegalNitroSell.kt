cd3739c79371348f3a554b2731cf7894b86cb468
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-11-05 11:12:26 -0300

    Change channel and add more messages

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/checkillegalnitrosell/CheckIllegalNitroSell.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/checkillegalnitrosell/CheckIllegalNitroSell.kt
@@ -4,7 +4,6 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.utils.MarkdownSanitizer
 import net.perfectdreams.loritta.helper.LorittaHelper
 import net.perfectdreams.loritta.helper.toNaiveBayesClassifier
-import net.perfectdreams.loritta.helper.utils.Constants
 
 /**
  * Checks if a message contains *bad* stuff about trading sonhos to Nitro
@@ -45,7 +44,7 @@ class CheckIllegalNitroSell {
         val predictedCategory = nbc.predictWithProbability(input)
 
         if (predictedCategory?.category == true && predictedCategory.probability >= 0.8) {
-            val channel = event.jda.getTextChannelById(Constants.PORTUGUESE_STAFF_CHANNEL_ID)
+            val channel = event.jda.getTextChannelById(773912391226359820L)
 
             channel?.sendMessage(
                     "<:lori_ban_hammer:741058240455901254> **|** ${event.author.asMention} tem ${"%.2f".format(predictedCategory.probability * 100).toDouble()}% de estar querendo vender Nitro por Sonhos na mensagem `${event.message.contentRaw}` <${event.message.jumpUrl}>! Verifique para mim!!"
