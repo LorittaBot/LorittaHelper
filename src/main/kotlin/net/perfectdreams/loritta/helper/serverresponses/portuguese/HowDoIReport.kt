001d10030718d11b39f140392a5c1c6528e18d52
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2020-12-24 14:03:17 -0300

    Add How do I report response.

---
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/HowDoIReport.kt
@@ -0,0 +1,27 @@
+package net.perfectdreams.loritta.helper.serverresponses.portuguese
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.perfectdreams.loritta.api.messages.LorittaReply
+import net.perfectdreams.loritta.helper.serverresponses.RegExResponse
+import net.perfectdreams.loritta.helper.utils.Emotes
+import java.util.regex.Pattern
+
+/**
+ * Response when user asks how to report users
+ */
+class HowDoIReport: RegExResponse() {
+
+    init {
+        patterns.add("onde|como".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("posso|".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("den(ú|u)nciar".toPattern(Pattern.CASE_INSENSITIVE))
+    }
+
+    override fun getResponse(event: GuildMessageReceivedEvent, message: String): List<LorittaReply> = listOf(
+        LorittaReply(
+            message = "Você pode fazer uma denúncia enviando `"denúncia"` no privado da <@!761931447207460864>, após fazer isto, ela irá te enviar um formulário com um link e código único, por isso não tire prints ou divulgue o seu código e o link.!",
+            prefix = "👮"
+        )
+    )
+
+}
