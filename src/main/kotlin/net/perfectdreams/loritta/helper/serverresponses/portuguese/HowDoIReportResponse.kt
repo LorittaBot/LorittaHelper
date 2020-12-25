4be1556126252b23966804882ff07e07a663ca31
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2020-12-25 15:11:12 -0300

    Edit HowDoIReport response.

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/HowDoIReportResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/HowDoIReportResponse.kt
@@ -3,6 +3,7 @@ package net.perfectdreams.loritta.helper.serverresponses.portuguese
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.perfectdreams.loritta.api.messages.LorittaReply
 import net.perfectdreams.loritta.helper.serverresponses.RegExResponse
+import net.perfectdreams.loritta.helper.utils.Emotes
 import java.util.regex.Pattern
 
 class HowDoIReportResponse: RegExResponse() {
@@ -15,8 +16,8 @@ class HowDoIReportResponse: RegExResponse() {
 
     override fun getResponse(event: GuildMessageReceivedEvent, message: String): List<LorittaReply> = listOf(
         LorittaReply(
-            message = "Você pode fazer uma denúncia enviando `'denúncia'` no privado da <@!761931447207460864>, após fazer isto, ela irá te enviar um formulário com um link e código único, por isso não tire prints ou divulgue o seu código e o link!",
-            prefix = "👮"
+            message = "Você pode fazer uma denúncia enviando `denúncia` no privado da <@!761931447207460864>, após fazer isto, ela irá te enviar um formulário com um link e código único, por isso não tire prints ou divulgue o seu código e o link!",
+            prefix = Emotes.LORI_BAN_HAMMER
         )
     )
 
