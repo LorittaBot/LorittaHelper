c04707b8c384c8d1137cdc605913009ef42d616a
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2021-01-05 16:06:54 -0300

    Improving "SendSonhos" response!

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/SendSonhosResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/SendSonhosResponse.kt
@@ -19,8 +19,18 @@ class SendSonhosResponse : RegExResponse() {
     override fun getResponse(event: GuildMessageReceivedEvent, message: String) =
         listOf(
             LorittaReply(
-                "Você pode enviar sonhos para uma pessoa utilizando `+pay @Usuário QuantidadeDeSonhos`",
+                "Você pode enviar sonhos para uma pessoa utilizando `+pay @Usuário QuantidadeDeSonhos`.",
                 Emotes.LORI_PAC
+            ),
+            LorittaReply(
+                "Não se esqueça, veja todas as regras da Loritta em https://loritta.website/guidelines, antes de fazer qualquer transação de sonhos! Você pode ser banido de utilizar a Lori se quebrar alguma dessas regras!",
+                mentionUser = false,
+                prefix = "<:lori_ban_hammer:741058240455901254>"
+            ),
+            LorittaReply(
+                "Ao aceitar a transação, você não conseguirá pedir os sonhos de volta e a equipe não irá te ajudar a pegar eles de volta, então apenas envie sonhos para pessoas confiáveis!",
+                mentionUser = false,
+                prefix = "<:sad_cat_thumbs_up:686370257308483612>"
             )
         )
 }
