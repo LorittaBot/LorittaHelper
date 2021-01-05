7f69649d6b1831ea618e61f2421be14d17ee859d
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2021-01-05 18:36:02 -0300

    Improving Helper responses! (#27)
    * Improving the response regex
    
    * Improving "ValorShip" response!
    
    * Improving "SendSonhos" response!
    
    * Improving the response content and the regex.
    
    * Adding new information in the regex and response
    
    * Improving the response content and the regex.
    
    * Fixing match problems
    
    * Adding 'true' in message.contains(...)

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/AboutMeResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/AboutMeResponse.kt
@@ -8,15 +8,19 @@ import java.util.regex.Pattern
 
 /**
  * Response when people ask about Loritta's
- * punishments system
+ * profile 'about me'
  */
 class AboutMeResponse : RegExResponse() {
+    override fun handleResponse(message: String): Boolean = patterns.all {
+        it.matcher(message).find()
+    } && !message.contains("falar", true)
+
     init {
         patterns.add("quero|onde|como".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("eu|".toPattern(Pattern.CASE_INSENSITIVE))
-        patterns.add("((alter|mud|fa(ç|ss)|troc)(a|o|ar))  ?(p(ra|ara) (alter|mud)(a|o|ar)|)".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("((alter|mud|fa(ç|ss)|troc)(a|o|ar)|fa(s|z))  ?(p(ra|ara) (alter|mud)(a|o|ar)|)".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("a|o|m(eu|inha)|".toPattern(Pattern.CASE_INSENSITIVE))
-        patterns.add("descri(c|ç(a|ã)o)|biografia|sobre?.mim".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("descri(c|ç(a|ã)o)|biografia|sobre?.mim|texto|messagem".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("d(e|o|a)|".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("m(eu|inha)|".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("p(erfil|rofile)|lo(rri|ri|)(ta|tta)|".toPattern(Pattern.CASE_INSENSITIVE))
