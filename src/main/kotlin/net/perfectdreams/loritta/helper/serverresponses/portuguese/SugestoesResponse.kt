b32e1ecdac0e9b4dd1463a8394b703f1fe57f66e
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2021-01-05 16:08:36 -0300

    Improving the response content and the regex.

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/SugestoesResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/SugestoesResponse.kt
@@ -11,14 +11,27 @@ import java.util.regex.Pattern
  */
 class SugestoesResponse : RegExResponse() {
     init {
-        patterns.add(WHERE_IT_IS_PT.toPattern(Pattern.CASE_INSENSITIVE))
-        patterns.add("sugest(ã|a)o|sugere".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("mo|onde|qual|existe|tem( )?jeito|ajuda|quer(o|ia)|tem algum ?(jeito|(ch|x)at))".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("eu( )?posso|de|fa(ço|z|s(er|))|pra|".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("fazer( uma)?|d(á|a)|".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("suge(st(ã|a)o|re|rir)".toPattern(Pattern.CASE_INSENSITIVE))
     }
 
     override fun getResponse(event: GuildMessageReceivedEvent, message: String) = listOf(
         LorittaReply(
-            "Você pode sugerir novas coisas no nosso servidor de comunidade da Loritta! <#761625835043291146>",
-            prefix = "⭐"
+            "Você pode sugerir novas coisas no nosso servidor de comunidade da Loritta! <#761625835043291146>, no canal <#359139508681310212>!",
+            mentionUser = false,
+            prefix = "<:lori_wow:626942886432473098>"
+        ),
+        LorittaReply(
+            "Antes de sugerir, verifique se a sua sugestão está no <#588449583781118009> (Canal de sugestões que serão ou não, implementadas na Lori).",
+            mentionUser = false,
+            prefix = "<a:lori_yay_wobbly:638040459721310238>"
+        ),
+        LorittaReply(
+            "E não se esqueça de ler o tópico do canal para não fazer sugestões bobas, ou para não quebrar nenhuma regra no canal!",
+            mentionUser = false,
+            prefix = Emotes.LORI_BAN_HAMMER
         )
     )
 }
