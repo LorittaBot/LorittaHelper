34676286d3d0324ba39cd1868f7bab93505ea303
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2021-01-05 14:40:43 -0300

    Adding new information in the regex and response

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/ReceiveSonhosResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/ReceiveSonhosResponse.kt
@@ -14,23 +14,34 @@ class ReceiveSonhosResponse : RegExResponse() {
     override val priority = -1
 
     init {
-        patterns.add("conseg|peg|ganh".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("(pra ?que|como)|(conseg|peg|ganh)".toPattern(Pattern.CASE_INSENSITIVE))
+        patterns.add("(us|serv)(e|o|a|ar)|".toPattern(Pattern.CASE_INSENSITIVE))
         patterns.add("sonhos".toPattern(Pattern.CASE_INSENSITIVE))
     }
 
     override fun getResponse(event: GuildMessageReceivedEvent, message: String) =
             listOf(
                     LorittaReply(
-                            "**Você pode conseguir sonhos... dormindo!**",
-                            Emotes.LORI_PAC
+                            "Opa! Parece que você tem uma dúvida sobre sonhos certo? **Mas você sabe o que são sonhos?** Sonhos é a moeda oficial do meu sistema de economia.",
+                            prefix = "<:lori_what:626942886361038868>"
                     ),
                     LorittaReply(
-                            "Brincadeirinha!! ^-^ Você pode pegar sonhos usando `+daily`",
+                            "**Tá mas o que eu faço com sonhos?** Simples! Com sonhos você pode desde apostar com seu amigo, até mesmo embelezar seu perfil comprando backgrounds! Demais né?",
+                            prefix = "<:lori_rica:593979718919913474>",
+                            mentionUser = false
+                    ),
+                    LorittaReply(
+                            "**Ok... Como eu consigo sonhos?** Você pode conseguir sonhos... dormindo!",
+                            prefix = Emotes.LORI_PAC,
+                            mentionUser = false
+                    ),
+                    LorittaReply(
+                            "Brincadeirinha!! ^-^ Você pode pegar sonhos usando `+daily`, mas lembrando que, sonhos não são criptomoedas, então não saia por aí vendendo como se fosse.",
                             prefix = Emotes.LORI_OWO,
                             mentionUser = false
                     ),
                     LorittaReply(
-                            "Se você quer saber outros jeitos de ganhar e gastar sonhos, veja o <#761337893951635458>",
+                            "Se você quer saber outros jeitos de ganhar e gastar sonhos, veja a nossa mensagem no <#761337893951635458>, clique aqui para voar até ela: https://discord.com/channels/420626099257475072/761337893951635458/762003311892103189",
                             prefix = Emotes.LORI_COFFEE,
                             mentionUser = false
                     )
