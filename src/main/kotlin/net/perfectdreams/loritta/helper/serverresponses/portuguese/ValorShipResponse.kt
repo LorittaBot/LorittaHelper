a91a5ef4fdb23782275cb22bd3cb6301a16bc278
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2021-01-05 16:04:41 -0300

    Improving "ValorShip" response!

--- src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/ValorShipResponse.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/serverresponses/portuguese/ValorShipResponse.kt
@@ -22,9 +22,24 @@ class ValorShipResponse : RegExResponse() {
             prefix = "❤️"
         ),
         LorittaReply(
-            "Maaaaaas se você quiser alterar o valor sem ficar mudando o seu nome no Discord, você pode subornar o deus do amor no meu website! Ah, e não se esqueça que custa sonhos subornar :3 <https://loritta.website/user/@me/dashboard/ship-effects>",
+            "Maaaaaas se você quiser alterar o valor sem ficar mudando o seu nome no Discord, você pode subornar o deus do amor no meu website! Ah, e não se esqueça que o suborno tem um custo de sonhos :3 <https://loritta.website/user/@me/dashboard/ship-effects>",
             mentionUser = false,
             prefix = Emotes.LORI_HEART
+        ),
+        LorittaReply(
+            "Se por acaso no meio do processo o website retorne um erro, o famoso `Unknown user`, tente usar o ID do usuário no lugar da sua tag.",
+            mentionUser = false,
+            prefix = "<:lori_very_owo:562303822978875403>"
+        ),
+        LorittaReply(
+            "Lembrando que ID e tag são coisas diferentes (`297153970613387264` é um **ID** e `Loritta#0219` é uma **tag**).",
+            mentionUser = false,
+            prefix = "<:lori_wow:626942886432473098>"
+        ),
+        LorittaReply(
+            "Saiba como pegar o ID de uma conta aqui: https://discord.com/channels/420626099257475072/761337893951635458/761572663478321182",
+            mentionUser = false,
+            prefix = Emotes.LORI_COFFEE
         )
     )
 }
