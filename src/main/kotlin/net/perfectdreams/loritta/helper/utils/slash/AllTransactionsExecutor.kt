423e410fb1c4c8164a3bcba58e1c768405581296
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-08-30 17:43:01 -0300

    Derp

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/AllTransactionsExecutor.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/AllTransactionsExecutor.kt
@@ -1,15 +1,14 @@
 package net.perfectdreams.loritta.helper.utils.slash
 
-import net.perfectdreams.discordinteraktions.api.entities.User
 import net.perfectdreams.discordinteraktions.common.context.commands.ApplicationCommandContext
 import net.perfectdreams.discordinteraktions.common.context.commands.slash.SlashCommandArguments
 import net.perfectdreams.discordinteraktions.declarations.commands.slash.SlashCommandExecutorDeclaration
 import net.perfectdreams.discordinteraktions.declarations.commands.slash.options.CommandOptions
 import net.perfectdreams.loritta.helper.LorittaHelper
-import net.perfectdreams.loritta.helper.tables.Dailies
 import net.perfectdreams.loritta.helper.tables.SonhosTransaction
 import net.perfectdreams.loritta.helper.utils.Constants
 import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.or
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Instant
@@ -19,42 +18,30 @@ class AllTransactionsExecutor(helper: LorittaHelper) : HelperSlashExecutor(helpe
         override val options = Options
 
         object Options : CommandOptions() {
-            init {
-                repeat(25) {
-                    optionalUser("user", "Usuário para ver as transações")
-                        .register()
-                }
-            }
+            val user = user("user", "Usuário para ver as transações")
+                .register()
         }
     }
 
     override suspend fun executeHelper(context: ApplicationCommandContext, args: SlashCommandArguments) {
         context.deferChannelMessage()
 
-        val users = args.types
-            .filter { it is User }
-            .mapNotNull { it.value }
-            .filterIsInstance<User>()
+        val user = args[options.user]
 
-        val dailies = transaction(helper.databases.lorittaDatabase) {
-            Dailies.select {
-                Dailies.receivedById inList users.map { it.id.value }
+        val transactions = transaction(helper.databases.lorittaDatabase) {
+            SonhosTransaction.select {
+                (SonhosTransaction.receivedBy eq user.id.value) or (SonhosTransaction.givenBy eq user.id.value)
             }.orderBy(SonhosTransaction.id, SortOrder.DESC)
                 .toList()
         }
 
         val builder = StringBuilder()
 
-        for (daily in dailies) {
-            val whenTheTransactionHappened = Instant.ofEpochMilli(daily[Dailies.receivedAt])
+        for (transaction in transactions) {
+            val whenTheTransactionHappened = Instant.ofEpochMilli(transaction[SonhosTransaction.givenAt])
                 .atZone(Constants.TIME_ZONE_ID)
 
-            builder.append("[${whenTheTransactionHappened.format(Constants.PRETTY_DATE_FORMAT)}] ${daily[Dailies.receivedById]}: ${daily[SonhosTransaction.quantity]} sonhos")
-            builder.append("- Email: ${daily[Dailies.email]}")
-            builder.append("\n")
-            builder.append("- IP: ${daily[Dailies.ip]}")
-            builder.append("\n")
-            builder.append("- User-Agent: ${daily[Dailies.userAgent]}")
+            builder.append("[${whenTheTransactionHappened.format(Constants.PRETTY_DATE_FORMAT)}/${transaction[SonhosTransaction.reason]}] ${transaction[SonhosTransaction.givenBy]} -> ${transaction[SonhosTransaction.receivedBy]} (${transaction[SonhosTransaction.quantity]} sonhos)")
             builder.append("\n")
         }
 
--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/DailyCheckExecutor.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/DailyCheckExecutor.kt
@@ -3,21 +3,66 @@ package net.perfectdreams.loritta.helper.utils.slash
 import net.perfectdreams.discordinteraktions.common.context.commands.ApplicationCommandContext
 import net.perfectdreams.discordinteraktions.common.context.commands.slash.SlashCommandArguments
 import net.perfectdreams.discordinteraktions.declarations.commands.slash.SlashCommandExecutorDeclaration
+import net.perfectdreams.discordinteraktions.declarations.commands.slash.options.CommandOptions
 import net.perfectdreams.loritta.helper.LorittaHelper
-import kotlin.concurrent.thread
+import net.perfectdreams.loritta.helper.tables.Dailies
+import net.perfectdreams.loritta.helper.tables.SonhosTransaction
+import net.perfectdreams.loritta.helper.utils.Constants
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.Instant
 
 class DailyCheckExecutor(helper: LorittaHelper) : HelperSlashExecutor(helper) {
     companion object : SlashCommandExecutorDeclaration(DailyCheckExecutor::class) {
+        override val options = Options
 
+        object Options : CommandOptions() {
+            init {
+                // Register 25 different users
+                repeat(25) {
+                    optionalUser("user", "Usuário para ver as transações")
+                        .register()
+                }
+            }
+        }
     }
 
     override suspend fun executeHelper(context: ApplicationCommandContext, args: SlashCommandArguments) {
-        context.sendEphemeralMessage {
-            content = "Verificando contas fakes..."
+        context.deferChannelMessage()
+
+        // Because we did stuff in a... unconventional way, we will get all matched user arguments in a unconventional way: By getting all resolved objects!
+        val users = context.data.resolved?.users?.values ?: run {
+            context.sendMessage {
+                content = "Nenhum usuário encontrado!"
+            }
+            return
         }
 
-        thread {
-            helper.dailyCatcherManager?.doReports()
+        val dailies = transaction(helper.databases.lorittaDatabase) {
+            Dailies.select {
+                Dailies.receivedById inList users.map { it.id.value }
+            }.orderBy(SonhosTransaction.id, SortOrder.DESC)
+                .toList()
+        }
+
+        val builder = StringBuilder()
+
+        for (daily in dailies) {
+            val whenTheTransactionHappened = Instant.ofEpochMilli(daily[Dailies.receivedAt])
+                .atZone(Constants.TIME_ZONE_ID)
+
+            builder.append("[${whenTheTransactionHappened.format(Constants.PRETTY_DATE_FORMAT)}] ${daily[Dailies.receivedById]}: ${daily[SonhosTransaction.quantity]} sonhos")
+            builder.append("- Email: ${daily[Dailies.email]}")
+            builder.append("\n")
+            builder.append("- IP: ${daily[Dailies.ip]}")
+            builder.append("\n")
+            builder.append("- User-Agent: ${daily[Dailies.userAgent]}")
+            builder.append("\n")
+        }
+
+        context.sendMessage {
+            file("transactions.txt", builder.toString().toByteArray(Charsets.UTF_8).inputStream())
         }
     }
 }
