5e2c426432abdfe999d3ecd71efb5b86e59c16fd
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-09-20 15:40:12 -0300

    Update Kotlin and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.30"
-    kotlin("plugin.serialization") version "1.4.30"
+    kotlin("jvm") version "1.5.21"
+    kotlin("plugin.serialization") version "1.5.21"
 }
 
 group = "net.perfectdreams.loritta.helper"
@@ -19,14 +19,14 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
-    implementation("io.github.microutils:kotlin-logging:1.8.3")
+    implementation("io.github.microutils:kotlin-logging:2.0.11")
 
-    implementation("net.dv8tion:JDA:4.3.0_313")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.4.1")
-    implementation("com.github.ben-manes.caffeine:caffeine:2.8.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.1.0")
+    implementation("net.dv8tion:JDA:4.3.0_330")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.2")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.0.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.2.2")
 
     // Kord
     implementation("dev.kord:kord-rest:0.8.x-SNAPSHOT")
@@ -38,18 +38,23 @@ dependencies {
     // We use the Gateway Kord impl because Gateway JDA is disabled for now, so we will convert the raw gateway events to Kord events
     implementation("net.perfectdreams.discordinteraktions:gateway-kord:0.0.9-SNAPSHOT")
 
+    // Used to serialize state on components
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.2.2")
+    // Used to serialize state on components
+    implementation("io.github.netvl.ecoji:ecoji:1.0.0")
+
     // Database
-    implementation("org.postgresql:postgresql:42.2.14")
-    implementation("com.zaxxer:HikariCP:3.4.5")
-    implementation("org.jetbrains.exposed:exposed-core:0.33.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.33.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.33.1")
+    implementation("org.postgresql:postgresql:42.2.23")
+    implementation("com.zaxxer:HikariCP:5.0.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.34.2")
+    implementation("org.jetbrains.exposed:exposed-dao:0.34.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.34.2")
 
     implementation("com.github.pemistahl:lingua:6a6d284145")
     testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 
-    implementation("io.ktor:ktor-client-cio:1.4.0")
+    implementation("io.ktor:ktor-client-cio:1.6.3")
 
     implementation("org.apache.commons:commons-text:1.9")
 }
