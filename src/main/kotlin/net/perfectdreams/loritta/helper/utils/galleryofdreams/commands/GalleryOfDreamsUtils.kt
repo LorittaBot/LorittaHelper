988e2401004c598136628766440a224d50b2eceb
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-02-02 00:07:57 -0300

    Fix fan art artist present in database check

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/galleryofdreams/commands/GalleryOfDreamsUtils.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/galleryofdreams/commands/GalleryOfDreamsUtils.kt
@@ -22,13 +22,13 @@ object GalleryOfDreamsUtils {
         val selectedAttachment = attachments.firstOrNull { it.id == data.selectedAttachmentId }
 
         content = if (selectedAttachment != null) {
-            if (data is AddFanArtToExistingArtistData) {
+            if (data is AddFanArtToNewArtistData) {
                 "**(Artista que não está no banco de dados da Galeria dos Sonhos! O artista será criado na galeria dos sonhos ao enviar a fan art)** Configure as informações da Fan Art! Selecionada: ${selectedAttachment.url}"
             } else {
                 "Configure as informações da Fan Art! Selecionada: ${selectedAttachment.url}"
             }
         } else {
-            if (data is AddFanArtToExistingArtistData) {
+            if (data is AddFanArtToNewArtistData) {
                 "**(Artista que não está no banco de dados da Galeria dos Sonhos! O artista será criado na galeria dos sonhos ao enviar a fan art)** Configure as informações da Fan Art!"
             } else {
                 "Configure as informações da Fan Art!"
