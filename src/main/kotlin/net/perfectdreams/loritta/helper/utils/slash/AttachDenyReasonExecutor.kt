2395a00c7e4412d89a2b44e5892be0b0ea467b97
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-02-05 21:31:34 -0300

    Allow support roles to use "/attachdenyreason"

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/AttachDenyReasonExecutor.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/AttachDenyReasonExecutor.kt
@@ -12,7 +12,7 @@ import net.perfectdreams.loritta.helper.utils.extensions.await
 import net.perfectdreams.loritta.helper.utils.generateserverreport.GenerateAppealsReport
 import net.perfectdreams.loritta.helper.utils.generateserverreport.GenerateServerReport
 
-class AttachDenyReasonExecutor(helper: LorittaHelperKord, val jda: JDA) : HelperSlashExecutor(helper, PermissionLevel.ADMIN) {
+class AttachDenyReasonExecutor(helper: LorittaHelperKord, val jda: JDA) : HelperSlashExecutor(helper, PermissionLevel.HELPER) {
     companion object : SlashCommandExecutorDeclaration(AttachDenyReasonExecutor::class) {
         override val options = Options
         val VALID_CHANNEL_IDS = listOf(
