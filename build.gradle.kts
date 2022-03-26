9c02aaf5c0027bd932b9bc91b32c6bbc8d73a797
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-03-26 20:21:17 -0300

    pls work

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ dependencies {
     api("net.perfectdreams.i18nhelper.formatters:icu-messageformat-jvm:0.0.3-SNAPSHOT")
 
     // GalleryOfDreams client
-    implementation("net.perfectdreams.galleryofdreams:common:1.0.3-20220326.222724-9")
-    implementation("net.perfectdreams.galleryofdreams:client:1.0.3-20220326.222724-9")
+    implementation("net.perfectdreams.galleryofdreams:common:1.0.3-SNAPSHOT")
+    implementation("net.perfectdreams.galleryofdreams:client:1.0.3-SNAPSHOT")
 
     // Used for the LocaleManager
     implementation("org.yaml:snakeyaml:1.29")
