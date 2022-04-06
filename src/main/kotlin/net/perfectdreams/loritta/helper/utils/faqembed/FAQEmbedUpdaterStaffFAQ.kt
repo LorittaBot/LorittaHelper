1f16271f53f2e5a3e7e95897c79d50b9ce75a7e2
PeterStark000 <40671520+PeterStark000@users.noreply.github.com>
2022-04-06 15:59:36 -0300

    Fix error in Staff FAQEmbedUpdater

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/faqembed/FAQEmbedUpdaterStaffFAQ.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/faqembed/FAQEmbedUpdaterStaffFAQ.kt
@@ -3,7 +3,7 @@ package net.perfectdreams.loritta.helper.utils.faqembed
 import net.dv8tion.jda.api.JDA
 import net.perfectdreams.loritta.helper.LorittaHelper
 
-class FAQEmbedUpdaterPortuguese(m: LorittaHelper, jda: JDA) : FAQEmbedUpdater(m, jda) {
+class FAQEmbedUpdaterStaffFAQ(m: LorittaHelper, jda: JDA) : FAQEmbedUpdater(m, jda) {
     override val title = "FAQ da Staff"
     override val channelId = 961059506840809473L
 }
