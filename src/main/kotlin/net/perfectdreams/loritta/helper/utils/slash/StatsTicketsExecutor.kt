56202c46b78246a09fdf5524bdaaf66b10063ece
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-08-14 01:17:18 -0300

    Allow filtering the tickets stats by system

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/StatsTicketsExecutor.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/slash/StatsTicketsExecutor.kt
@@ -6,7 +6,9 @@ import net.perfectdreams.discordinteraktions.common.commands.options.Application
 import net.perfectdreams.discordinteraktions.common.commands.options.SlashCommandArguments
 import net.perfectdreams.discordinteraktions.common.utils.footer
 import net.perfectdreams.loritta.helper.LorittaHelperKord
+import net.perfectdreams.loritta.helper.tables.StartedSupportSolicitations
 import net.perfectdreams.loritta.helper.tables.TicketMessagesActivity
+import net.perfectdreams.loritta.helper.utils.tickets.TicketUtils
 import org.jetbrains.exposed.sql.countDistinct
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -14,6 +16,20 @@ import java.time.Instant
 
 class StatsTicketsExecutor(helper: LorittaHelperKord) : HelperSlashExecutor(helper, PermissionLevel.HELPER) {
     inner class Options : ApplicationCommandOptions() {
+        val system = string("system", "Sistema") {
+            for (type in TicketUtils.TicketSystemType.values()) {
+                choice(
+                    when (type) {
+                        TicketUtils.TicketSystemType.HELP_DESK_PORTUGUESE -> "Loritta Help Desk (Português)"
+                        TicketUtils.TicketSystemType.HELP_DESK_ENGLISH -> "Loritta Help Desk (English)"
+                        TicketUtils.TicketSystemType.FIRST_FAN_ARTS_PORTUGUESE -> "Primeira Fan Art (Português)"
+                        TicketUtils.TicketSystemType.SPARKLYPOWER_HELP_DESK_PORTUGUESE -> "SparklyPower Help Desk"
+                    },
+                    type.name
+                )
+            }
+        }
+
         val filter = optionalString("filter", "Filtro de data") {
             choice("Últimos 7 dias", "7")
             choice("Últimos 14 dias", "14")
@@ -26,6 +42,7 @@ class StatsTicketsExecutor(helper: LorittaHelperKord) : HelperSlashExecutor(help
     override val options = Options()
 
     override suspend fun executeHelper(context: ApplicationCommandContext, args: SlashCommandArguments) {
+        val system = TicketUtils.TicketSystemType.valueOf(args[options.system])
         val filterDay = args[options.filter]
 
         // When using Instant.MIN, this happened:
@@ -38,9 +55,12 @@ class StatsTicketsExecutor(helper: LorittaHelperKord) : HelperSlashExecutor(help
         val result = transaction(helper.databases.helperDatabase) {
             val resultCount = TicketMessagesActivity.supportSolicitationId.countDistinct()
 
-            val currentBanStatus = TicketMessagesActivity.slice(TicketMessagesActivity.userId, resultCount).select {
-                TicketMessagesActivity.timestamp greaterEq since
-            }
+            val currentBanStatus = TicketMessagesActivity
+                .innerJoin(StartedSupportSolicitations)
+                .slice(TicketMessagesActivity.userId, resultCount)
+                .select {
+                    TicketMessagesActivity.timestamp greaterEq since
+                }
                 .groupBy(TicketMessagesActivity.userId)
                 .toList()
 
@@ -55,6 +75,8 @@ class StatsTicketsExecutor(helper: LorittaHelperKord) : HelperSlashExecutor(help
 
 
         context.sendMessage {
+            content = "Sistema: $system"
+            
             embed {
                 title = "Ranking de Pessoas Tagarelas em Tickets Respondidos"
 
