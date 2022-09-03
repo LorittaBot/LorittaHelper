33226af27bf19be466807c7ce9b51b685d50b64b
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-09-03 14:48:59 -0300

    Revert the locked change

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/tickets/CreateTicketButtonExecutor.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/tickets/CreateTicketButtonExecutor.kt
@@ -170,7 +170,8 @@ class CreateTicketButtonExecutor(val m: LorittaHelperKord) : ButtonExecutor {
                     this.archived = false
                     // Before we used locked = false due to a bug in Discord Mobile related to "You don't have permission!", however it seems to have been fixed, so we don't need to keep it unlocked
                     // Besides, allowing unlock makes people confused, because they click to close the ticket but they can still send messages in the ticket
-                    this.locked = true
+                    // Update: We are now using locked again because it is still broken lmao
+                    this.locked = false
                     this.invitable = false
                 }.toRequest(),
                 "Unarchival request via button by ${user.username}#${user.discriminator} (${user.id.value})"
