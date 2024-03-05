6bad08bdb4436efbe7ae6b9d50dc6a038c9b6b65
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-03-05 19:29:07 -0300

    Revert "Change portuguese ticket channel due to a Discord bug where the old channel is not showing up in the channels & roles section"
    This reverts commit d1887e13a63c91a13a68ff503c49c2341bf3aa36.

--- src/main/kotlin/net/perfectdreams/loritta/helper/utils/tickets/TicketUtils.kt
+++ src/main/kotlin/net/perfectdreams/loritta/helper/utils/tickets/TicketUtils.kt
@@ -10,7 +10,7 @@ import net.perfectdreams.loritta.helper.utils.tickets.systems.LorittaHelpDeskTic
 import net.perfectdreams.loritta.helper.utils.tickets.systems.SparklyPowerHelpDeskTicketSystem
 
 class TicketUtils(val m: LorittaHelper) {
-    private val PORTUGUESE_HELP_DESK_CHANNEL_ID = 1214690015620108338L
+    private val PORTUGUESE_HELP_DESK_CHANNEL_ID = 1077726822160142386L
     private val ENGLISH_HELP_DESK_CHANNEL_ID = 891834950159044658L
     private val FIRST_FAN_ART_CHANNEL_ID = 938247721775661086L
     private val SPARKLYPOWER_HELP_DESK_CHANNEL_ID = 994664055933517925L
@@ -22,7 +22,7 @@ class TicketUtils(val m: LorittaHelper) {
             TicketSystemType.HELP_DESK_PORTUGUESE,
             LanguageName.PORTUGUESE,
             Constants.COMMUNITY_SERVER_ID,
-            1214690015620108338L,
+            1077726822160142386L,
             PortugueseResponses.responses,
             1191767752894062632L,
             610094449737072660L,
